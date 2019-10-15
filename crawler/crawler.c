be7e640ed9811ee60fd5106079ef002902890bc9
Satch Baker <samuel.h.baker.22@dartmouth.edu>
2019-10-15 17:56:36 -0400

    2nd commit, added crawler.c

---
+++ crawler/crawler.c
@@ -0,0 +1,14 @@
+/* crawler.c --- 
+ * 
+ * 
+ * Author: Samuel H. Baker
+ * Created: Tue Oct 15 17:50:46 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+void main() {
+	printf("hello");
+}
